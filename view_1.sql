c1488918d80e28b19b96d54488a21d44c7b18d12
Salvatore <pigrecoinfinito@gmail.com>
2016-12-26 17:36:20 +0100

    add commenti

--- view_1.sql
+++ view_1.sql
@@ -1,8 +1,8 @@
 select g.id as id_grafo, p.pk_uid as pk_uid_pill
-from (select gs.id,st_buffer (gs.geom, 0.01) as geom from grafo_stradale gs) g,
+from (select gs.id,st_buffer (gs.geom, 0.01) as geom from grafo_stradale gs) g, -- calcolo un buffer sul grafo
      (select pt.pk_uid, closestpoint (l.geom, pt.geom) as geometry 
       from pti_pill pt, grafo_stradale l
       group by pt.pk_uid
-      having min ( st_length(shortestline (pt.geom, l.geom)))) p
-where st_intersects (p.geometry, g.geom) = 1
-order by p.pk_uid
+      having min ( st_length(shortestline (pt.geom, l.geom)))) p -- calcolo i punti closestpoint
+where st_intersects (p.geometry, g.geom) = 1 -- solo feature che si intersecano 
+order by p.pk_uid -- ordino per 
